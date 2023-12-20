5d02f22fd1339421e8d04c053df530a230138181
naikymen <you@example.com>
2023-12-20 00:56:34 -0300

    update header

--- klippy/kinematics/cartesian_abc.py
+++ klippy/kinematics/cartesian_abc.py
@@ -1,7 +1,8 @@
-# Code for handling the kinematics of cartesian robots
+# Code for handling the kinematics of CoreXY robots
 #
 # Copyright (C) 2016-2021  Kevin O'Connor <kevin@koconnor.net>
-#
+# Copyright (C) 2023-2024  Nicolás A. Méndez
+# 
 # This file may be distributed under the terms of the GNU GPLv3 license.
 
 # Type checking without cyclicc import error.
