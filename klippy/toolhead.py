d3806a9ecf43e79007f3b0ca1f93c07add9c55c7
naikymen <you@example.com>
2024-03-08 10:52:44 -0300

    add extruder to th axis_config

--- klippy/toolhead.py
+++ klippy/toolhead.py
@@ -454,7 +454,8 @@ class ToolHead:
         
         # Dictionary to map axes to their indexes in the position vector "self.commanded_pos".
         self.axis_map = {a: i for i, a in enumerate(list(self.ax_letters)[:self.min_axes] + ["E"])}
-        self.axis_config = [self.axis_map[x] for x in self.axis_names]
+        # Full set of configured axes indexes, including the extruder.
+        self.axis_config = [self.axis_map[x] for x in (self.axis_names + "E")]
         logging.info(f"ToolHead: setup axis_map to '{self.axis_map}' and axis_config to '{self.axis_config}'.")
 
         # Which of the kinematic (non-extruder) axes are limited by the general acceleration setting.
